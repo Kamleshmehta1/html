ca0129a8ff472162367587e24f56231c369535aa
KAMLESH MEHTA <kamleshm1999@gmail.com>
2022-01-05 22:34:40 +0530

    o(n) funda

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -1,20 +1,32 @@
 //bubble sort
 
 function bubbleSort(arr) {
-    for (let i = 0; i < arr.length; i++) {
-      for (let j = 0; j < arr.length - i - 1; j++) {
-        if (arr[j] > arr[j + 1]) {
-          // var temp = arr[j];
-          // arr[j] = arr[j + 1];
-          // arr[j + 1] = temp;
+  var alreadySorted = true;
 
-          [arr[j+1],arr[j]]=[arr[j],arr[j+1]];
-        }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > arr[i + 1]) {
+      alreadySorted = false;
+    }
+
+    if (alreadySorted == true) {
+      console.log(alreadySorted);
+      return arr;
+    }
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // var temp = arr[j];
+        // arr[j] = arr[j + 1];
+        // arr[j + 1] = temp;
+
+        [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]];
       }
     }
-    return arr;
   }
-  
-  var arr = [64, 34, 25, 12, 22, 11, 90];
-  console.log(bubbleSort(arr));
-  
+  return arr;
+}
+
+var arr = [64, 34, 25, 12, 22, 11, 90];
+console.log(bubbleSort(arr));
