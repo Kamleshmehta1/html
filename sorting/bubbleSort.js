54a13aef71e2d6e16210f110d7c2ed855f81a968
KAMLESH MEHTA <kamleshm1999@gmail.com>
2022-01-06 00:54:32 +0530

    done

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -21,7 +21,7 @@ function bubbleSort(arr) {
         // arr[j] = arr[j + 1];
         // arr[j + 1] = temp;
 
-        [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]];
+        [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]]
       }
     }
   }
