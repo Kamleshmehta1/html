4bf77a784ae76ff5dfe8f0ec726d8783f268c693
KAMLESH MEHTA <kamleshm1999@gmail.com>
2022-01-05 23:01:31 +0530

    insertionsort

---
+++ sorting/insertionSort.js
@@ -0,0 +1,16 @@
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    //go thrugh the elemtnts behind the curent element and placed the current elements in the current place.
+    for (let j = i - 1; j >= 0; j--) {
+      if (arr[j + 1] < arr[j]) {
+        [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]];
+      }
+    }
+  }
+
+  return arr;
+}
+
+var arr = [23,1,10,5,2];
+
+console.log(insertionSort(arr));
