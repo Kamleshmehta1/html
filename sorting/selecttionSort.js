6a84faed3fdad7776f42440ad83b9b131c77b830
KAMLESH MEHTA <kamleshm1999@gmail.com>
2022-01-06 00:51:48 +0530

    selectionSort

---
+++ sorting/selecttionSort.js
@@ -0,0 +1,17 @@
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    if (min !== i) {
+      [arr[i], arr[min]] = [arr[min], arr[i]];
+    }
+  }
+  return arr;
+}
+
+let arr = [10, 9, 8, 3, 2, 5, 4, 7];
+console.log(selectionSort(arr));
